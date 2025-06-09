644c79875744e518fe4f2fdda78c3207abbb22cd
Marcus Santos <marcus.xtreme03@gmail.com>
2025-06-08 22:43:58 -0300

    :wrench: feat: Adicionando Filtragem por categoria no ContactAdmin

--- contact/admin.py
+++ contact/admin.py
@@ -5,15 +5,17 @@ from contact import models
   
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',) # Mostra as informações no Admin
-
+    search_fields = ['name']
 @admin.register(models.Contact)
   
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id','first_name', 'last_name', 'phone', 'email', 'create_date', 'description','category',) # Mostra as informações no Admin
     ordering = 'id',
     # list_filter = 'create_date', # Filtro por data
-    search_fields = ['id','first_name','last_name'] # Campo de busca
+    search_fields = ['id','first_name','last_name','category__name'] # Campo de busca
     list_per_page = 10
     list_max_show_all = 10 # Quantidade máxima de registros por paginação
     # list_editable = 'first_name', 'last_name' # Permite editar os campos
     list_display_links = 'id','first_name','phone' # Cria um link ao clicar para editar ocampo selecionado
+
+    
