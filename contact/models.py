bbee67333db6c7380284deb5cf2f2ae9d5e8e4c5
Marcus Santos <marcus.xtreme03@gmail.com>
2025-06-12 11:33:33 -0300

    :wrench: feat: Criação campo owner no Contact

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone # tempo atual
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
   name = models.CharField(max_length=50)
@@ -24,6 +26,7 @@ class Contact(models.Model):
   category = models.ForeignKey(Category,
   on_delete=models.SET_NULL,  # Tipos de on_delete => 1) CASCADE 2) SET_NULL 3) PROTECT 4 ) SET_DEFAULT
   blank=True,null=True)
+  owner = models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True)
   
   class Meta:
     verbose_name = 'Contact'
