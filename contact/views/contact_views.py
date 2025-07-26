fcb20b1bfae4b083f32c44b7060807890206c8b6
MarcuSantosDev <marcus.xtreme03@gmail.com>
2025-07-26 17:20:13 -0300

    :wrench: feat: Criação View contact

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -7,3 +7,10 @@ def index(request):
     'contacts': contacts,
   }
   return render(request, 'contact/index.html',context)
+
+def contact(request,contact_id):
+  single_contact = Contact.objects.get(pk=contact_id)
+  context = {
+    'contact': single_contact,
+  }
+  return render(request, 'contact/contact.html',context)
--- project/urls.py
+++ project/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings # para importar media url e static url
 
 urlpatterns = [
-    path('agenda', include('contact.urls')),
+    path('agenda/', include('contact.urls')),
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
