18a8ed9b95c2879f4940877b7a70a212ccd07fbb
MarcuSantosDev <marcus.xtreme03@gmail.com>
2025-07-27 16:30:49 -0300

    🥅 feat: Erro 404 para id´s inexistente

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from contact.models import Contact
+from django.http import Http404 
 
 def index(request):
   contacts = Contact.objects.filter(show=True).order_by('id')
@@ -9,7 +10,7 @@ def index(request):
   return render(request, 'contact/index.html',context)
 
 def contact(request,contact_id):
-  single_contact = Contact.objects.get(pk=contact_id)
+  single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id),show=True) # Se não houver id existente levanta erro 404
   context = {
     'contact': single_contact,
   }
