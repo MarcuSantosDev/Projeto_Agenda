bac279371d2f86ac894cb8606894e4ead97fde76
MarcuSantosDev <marcus.xtreme03@gmail.com>
2025-07-31 19:18:59 -0300

    :sparkles: feat: Filtrando valores com Q no search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,get_object_or_404
 from contact.models import Contact
 from django.http import Http404 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.db.models import Q
 
 @login_required
 def index(request):
@@ -10,6 +11,28 @@ def index(request):
   context = {
     'contacts': contacts,
     'site_title': 'Contatos - '
+  }
+  return render(request, 'contact/index.html',context)
+
+@login_required
+def search(request):
+  search_value = request.GET.get('q','').strip() # Se não encontrar a queryset volta string vazia
+  print('search_value',search_value)
+  
+  if search_value == '':
+    return redirect('contact:index')
+  print(search_value)
+  contacts = Contact.objects.filter(show=True).order_by('id').filter(
+    Q(first_name__icontains=search_value) |   # Classe Q permite usar or usando um "|"
+    Q(last_name__icontains=search_value) |
+    Q(phone__icontains=search_value) |
+    Q(email__icontains=search_value)
+    ) # __ icontains é um teste de contenção que não diferencia maiúsculas de minúsculas.
+
+  print(contacts.query)
+  context = {
+    'contacts': contacts,
+    'site_title': 'Site - '
   }
   return render(request, 'contact/index.html',context)
 
