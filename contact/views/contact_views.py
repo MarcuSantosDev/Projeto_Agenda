01ccd68c4f46f52b165a9b8832d5b2f151badbf7
MarcuSantosDev <marcus.xtreme03@gmail.com>
2025-07-31 14:03:09 -0300

    :lipstick: style: Exibição do nome do contato no titulo da pagina

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -6,12 +6,15 @@ def index(request):
   contacts = Contact.objects.filter(show=True).order_by('id')
   context = {
     'contacts': contacts,
+    'site_title': 'Contatos - '
   }
   return render(request, 'contact/index.html',context)
 
 def contact(request,contact_id):
   single_contact = get_object_or_404(Contact,pk=contact_id,show=True) # Se não houver id existente levanta erro 404
+  contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
   context = {
     'contact': single_contact,
+    'site_title':contact_name
   }
   return render(request, 'contact/contact.html',context)
